eedf845f4307b25b29488259bfb13b74dda65a25
Mario Dian <mariodian@gmail.com>
2026-04-15 22:52:15 +0800

    Fix version format in pincer.rb

--- Casks/pincer.rb
+++ Casks/pincer.rb
@@ -1,5 +1,5 @@
 cask "pincer" do
-  version "v0.3.1"
+  version "0.3.1"
   sha256 "261be2a040ec5eb5f6771d192a91618f2a1859eec22c42254661d5e5aab4d0b7"
 
   url "https://github.com/mariodian/pincer/releases/download/v#{version}/stable-macos-arm64-Pincer.dmg"
